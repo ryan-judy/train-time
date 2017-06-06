54df12614bbbad9dc6fd9f4a65f89a5ea9caa5a4
ryan-judy <rj113909@gmail.com>
2017-06-05 22:34:31 -0400

    failing

--- javascript/logic.js
+++ javascript/logic.js
@@ -10,6 +10,7 @@ var config = {
     messagingSenderId: "691902959472"
   };
 
+
   firebase.initializeApp(config);
 
 //Set database variable
@@ -19,69 +20,27 @@ var destination = "";
 var frequency = 0;
 var arrival = 0;
 var away;
-//var schedule = $("<td class = 'train-name'><td class = 'train-destination'><td class = 'train-frequency'><td class = 'train-arrival'><td class = 'train-away'>");
 var newGroup;
 var current;
 var ok;
 var currentObject;
 var number;
+var newBody;
+var schedule;
+var tName = $(".train-name");
+var tDestination = $(".train-destination");
+var tFrequency = $(".train-frequency");
+var tArrival = $(".train-arrival");
+var input;
 
-	//When submit button is pressed, push info to the train group.
-	$("#submit-button").on("click", function() {
+
+  //When submit button is pressed, push info to the train group.
+    database.ref().on("value", function(snapshot) {
+
+       $("#submit-button").on("click", function() {
       // Prevent the page from refreshing
-    	event.preventDefault();
-
-				       
-    	schedule = $(".train-schedule").append(schedule);
-		name = $("#name-input").val();
-	    destination = $("#destination-input").val().trim();
-	    arrival = $("#first-time-input").val().trim();
-	    frequency = $("#frequency-input").val().trim();
-
-	    database.ref().push({
-	        trainName: name,
-	        trainDestination: destination,
-	        trainArrival: arrival,
-	        trainFrequency: frequency,
-
-      	});
-
-
-
-	});
-
-	database.ref().on("value", function(snapshot) {
-
-      // storing the snapshot.val() in a variable for convenience
-      var data = snapshot.val();
-      var dataArr = Object.keys(data);
-      var thisObject = data[key];
-
-      for( var key in data ){
-            newBody = $("<tbody class = 'train-group'>");
-            var a = $(".table").append(newBody);
-            newTr = $("<tr>");
-            schedule = $("<td class = 'train-name'><td class = 'train-destination'><td class = 'train-frequency'><td class = 'train-arrival'><td class = 'train-away'>");
-            newTr.append(schedule);
-            newBody.append(newTr);
-  
-        	for (var i = 0; i < dataArr.length; i++) {
-            ok = dataArr[i];
-            currentObject = data[ok];
-      	   	$(".train-name").html(currentObject.trainName);
-  	        $(".train-destination").html(currentObject.trainDestination);
-  	        $(".train-frequency").html(currentObject.trainArrival);
-  	        $(".train-arrival").html(currentObject.trainFrequency);
-	          console.log(currentObject.trainName);
-      			console.log(currentObject.trainDestination);
-      			console.log(currentObject.trainArrival);
-      			console.log(currentObject.trainFrequency);
-
-      }
-  }
-  
-
- 	       // Getting an array of each key In the snapshot object
+      event.preventDefault();
+             // Getting an array of each key In the snapshot object
 
       // Finding the last user's key
       var lastIndex = dataArr.length - 1;
@@ -97,16 +56,65 @@ var number;
       console.log(lastObj.trainDestination);
       console.log(lastObj.trainArrival);
       console.log(lastObj.trainFrequency);
-
       
       //$(".train-name").html(lastObj.trainName);
       //$(".train-destination").html(lastObj.trainDestination);
       //$(".train-frequency").html(lastObj.trainFrequency);
       //$(".train-arrival").html(lastObj.trainArrival);
+               
+      name = $("#name-input").val();
+      destination = $("#destination-input").val().trim();
+      arrival = $("#first-time-input").val().trim();
+      frequency = $("#frequency-input").val().trim();
+      schedule = moment.duration(frequency).asMinutes("hh:mm a");
+      console.log(schedule)
+      input = moment(arrival, "HH:mm").format("hh:mm a");
+      database.ref().push({
+          trainName: name,
+          trainDestination: destination,
+          trainArrival: frequency,
+          trainFrequency: input,
+        });
+  });
+
+  
+      // storing the snapshot.val() in a variable for convenience
+      var data = snapshot.val();
+      var dataArr = Object.keys(data);
+      var thisObject = data[key];
+
+      for( var key in data ){
+      }
+        function myFunction() {
+          for (var i = 0; i < dataArr.length; i++) {
+             ok = dataArr[i];
+            currentObject = data[ok];
+            newBody = $("<tbody class = 'train-group'>");
+            var newTr = $("<tr>");
+            var nameTd = $("<td class = 'train-name'>");
+            var desintationTd = $("<td class = 'train-destination'>");
+            var frequencyTd = $("<td class = 'train-frequency'>");
+            var arrivalTd = $("<td class = 'train-arrival'>");
+            //var awayTd = $("<td class = 'train-away'>");
+              nameTd.append(currentObject.trainName);
+              desintationTd.append(currentObject.trainDestination);
+              frequencyTd.append(currentObject.trainArrival);
+              arrivalTd.html(currentObject.trainFrequency);
+
+              newTr.append(nameTd);
+              newTr.append(desintationTd);
+              newTr.append(frequencyTd);
+              newTr.append(arrivalTd);
+              newBody.append(newTr);
+            $(".table").append(newBody);
+        }
+      }
+
+      myFunction();
 
       // Handle the errors
     }, function(errorObject) {
       console.log("Errors handled: " + errorObject.code);
     });
+     });
 
-});
